031b359c85230226f8e92f0edff6cf11e03e4965
Sameer <sameerrahangdale63@gmail.com>
2025-01-15 10:28:51 +0530

    change style of div by using DOM

---
+++ script.js
@@ -0,0 +1,13 @@
+let box = document.querySelector(".main");
+let text = document.querySelector("#text")
+
+box.style.backgroundColor = 'green';
+box.style.margin = '15px';
+box.style.padding = '10px';
+box.style.height = '300px';
+box.style.width = '400px';
+box.style.border = '2px solid black';
+
+text.style.fontSize = '30px';
+text.style.fontWeight = '400';
+text.style.color = 'red';
